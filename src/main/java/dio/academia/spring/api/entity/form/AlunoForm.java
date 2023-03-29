0f2f8c993792d1fb40278b2fd59734479b5e9432
Matheus Victor <matheusvictor.salles@gmail.com>
2023-03-29 17:50:45 -0300

    fix: Adiciona notações
    - Notações responsáveis pelos métodos acessores e construtores

--- src/main/java/dio/academia/spring/api/entity/form/AlunoForm.java
+++ src/main/java/dio/academia/spring/api/entity/form/AlunoForm.java
@@ -1,14 +1,21 @@
 package dio.academia.spring.api.entity.form;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AlunoForm {
 
-  private String nome;
+    private String nome;
 
-  private String cpf;
+    private String cpf;
 
-  private String bairro;
+    private String bairro;
 
-  private LocalDate dataDeNascimento;
+    private LocalDate dataDeNascimento;
 }
