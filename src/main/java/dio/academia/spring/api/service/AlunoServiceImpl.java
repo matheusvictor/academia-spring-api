6331b28dcc43a1bbcd3e9edb3501b6eaf94393fd
Matheus Victor <matheusvictor.salles@gmail.com>
2023-03-29 17:53:41 -0300

    feat: Add implementações para camada de serviço
    - Criação de novo aluno
    - Listagem completa de alunos

---
+++ src/main/java/dio/academia/spring/api/service/AlunoServiceImpl.java
@@ -0,0 +1,49 @@
+package dio.academia.spring.api.service;
+
+import dio.academia.spring.api.entity.Aluno;
+import dio.academia.spring.api.entity.form.AlunoForm;
+import dio.academia.spring.api.entity.form.AlunoUpdateForm;
+import dio.academia.spring.api.repository.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AlunoServiceImpl implements IAlunoService {
+
+    @Autowired
+    private AlunoRepository repository;
+
+    @Override
+    public Aluno create(AlunoForm form) {
+        Aluno aluno = new Aluno();
+        aluno.setNome(form.getNome());
+        aluno.setCpf(form.getCpf());
+        aluno.setBairro(form.getBairro());
+        aluno.setDataDeNascimento(form.getDataDeNascimento());
+
+        this.repository.save(aluno);
+        return aluno;
+    }
+
+    @Override
+    public Aluno get(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Aluno> getAll() {
+        return this.repository.findAll();
+    }
+
+    @Override
+    public Aluno update(Long id, AlunoUpdateForm formUpdate) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
