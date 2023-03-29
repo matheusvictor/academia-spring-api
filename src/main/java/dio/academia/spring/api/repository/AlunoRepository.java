0718d12dff4ddf9c57b28117d7962f38a23b7710
Matheus Victor <matheusvictor.salles@gmail.com>
2023-03-29 17:52:01 -0300

    feat: Adiciona extensão do JpaRepository
    Permitirá utilizar implementações do JPA para a camada de Repository

--- src/main/java/dio/academia/spring/api/repository/AlunoRepository.java
+++ src/main/java/dio/academia/spring/api/repository/AlunoRepository.java
@@ -1,4 +1,9 @@
 package dio.academia.spring.api.repository;
 
-public interface AlunoRepository {
+import dio.academia.spring.api.entity.Aluno;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+@Repository
+public interface AlunoRepository extends JpaRepository<Aluno, Long> {
 }
