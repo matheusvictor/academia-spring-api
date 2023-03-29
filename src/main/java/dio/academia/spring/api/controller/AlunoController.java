ac7b294f41a7d23b3024168ee34ec6f39c6eb6d2
Matheus Victor <matheusvictor.salles@gmail.com>
2023-03-29 17:52:50 -0300

    feat: Add endpoints
    - Listagem completa
    - Cadastro de aluno

--- src/main/java/dio/academia/spring/api/controller/AlunoController.java
+++ src/main/java/dio/academia/spring/api/controller/AlunoController.java
@@ -1,9 +1,27 @@
 package dio.academia.spring.api.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import dio.academia.spring.api.entity.Aluno;
+import dio.academia.spring.api.entity.form.AlunoForm;
+import dio.academia.spring.api.service.AlunoServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/alunos")
 public class AlunoController {
+
+    @Autowired
+    private AlunoServiceImpl alunoService;
+
+    @PostMapping
+    public Aluno create(@RequestBody AlunoForm form) {
+        return this.alunoService.create(form);
+    }
+
+    @GetMapping
+    public List<Aluno> getAll() {
+        return alunoService.getAll();
+    }
 }
