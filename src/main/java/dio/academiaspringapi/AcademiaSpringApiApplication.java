10f10ae4814d3acb7b27479fa9f4bf32ef3e0850
Matheus Victor <matheusvictor.salles@gmail.com>
2023-03-29 14:19:38 -0300

    Commit inicial

---
+++ src/main/java/dio/academiaspringapi/AcademiaSpringApiApplication.java
@@ -0,0 +1,13 @@
+package dio.academiaspringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiaSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiaSpringApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/academiaspringapi/AcademiaSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.academiaspringapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiaSpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
