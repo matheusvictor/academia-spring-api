4666e3a224c5657135950e3023752168c7b9faf8
Matheus Victor <matheusvictor.salles@gmail.com>
2023-03-29 14:33:34 -0300

    feat: Add estrutura básica de controllers

---
+++ src/main/java/dio/academia/spring/api/controller/AvaliacaoFisicaController.java
@@ -0,0 +1,9 @@
+package dio.academia.spring.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/avaliacoes")
+public class AvaliacaoFisicaController {
+}
